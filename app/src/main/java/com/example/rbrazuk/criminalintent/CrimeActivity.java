213405292f58ae94f7cbd895b1c17de8d8607396
Ross Brazuk <rbrazuk@tonicdesign.com>
2016-03-11 10:45:26 -0500

    chapter 1 complete

--- app/src/main/java/com/example/rbrazuk/criminalintent/CrimeActivity.java
+++ app/src/main/java/com/example/rbrazuk/criminalintent/CrimeActivity.java
@@ -1,5 +1,7 @@
 package com.example.rbrazuk.criminalintent;
 
+import android.app.FragmentManager;
+import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -10,5 +12,13 @@ public class CrimeActivity extends FragmentActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        android.support.v4.app.FragmentManager fm = getSupportFragmentManager();
+        Fragment fragment = fm.findFragmentById(R.id.fragment_container);
+
+        if(fragment == null) {
+            fragment = new CrimeFragment();
+            fm.beginTransaction().add(R.id.fragment_container,fragment).commit();
+        }
     }
 }
