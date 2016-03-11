579f8b7fb42e71736f552b9db15687329dc2f218
Ross Brazuk <rbrazuk@tonicdesign.com>
2016-03-11 10:33:05 -0500

    added CrimeFragment

---
+++ app/src/main/java/com/example/rbrazuk/criminalintent/CrimeFragment.java
@@ -0,0 +1,28 @@
+package com.example.rbrazuk.criminalintent;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class CrimeFragment extends Fragment {
+
+    private Crime mCrime;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mCrime = new Crime();
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        View v = inflater.inflate(R.layout.fragment_crime,container,false);
+        return v;
+    }
+}
